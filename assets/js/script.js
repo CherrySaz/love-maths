6025e8a0d163ee1c13e340f1bbb00c20aa635e1d
CherrySaz <>
2023-08-07 16:46:00 +0000

    Add structure to game area

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+function IncrementScore() {
+ 
+}
+
+function IncrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion( ) {
+    
+}
